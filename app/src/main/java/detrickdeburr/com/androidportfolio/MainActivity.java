1108683eace906ca86808b8c36483e1944350dd8
Detrick DeBurr <detrick@digitalflow.com>
2015-11-06 05:57:52 -0600

    Initial commit

---
+++ app/src/main/java/detrickdeburr/com/androidportfolio/MainActivity.java
@@ -0,0 +1,93 @@
+package detrickdeburr.com.androidportfolio;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button button0;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        button0 = (Button)findViewById(R.id.button0);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        button4 = (Button)findViewById(R.id.button4);
+        button5 = (Button)findViewById(R.id.button5);
+
+        button0.setOnClickListener(this);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String sMsg="";
+        Button b = (Button)v;
+        switch (v.getId()){
+            case R.id.button0:
+                sMsg=b.getText().toString();
+                break;
+            case R.id.button1:
+                sMsg=b.getText().toString();
+                break;
+            case R.id.button2:
+                sMsg=b.getText().toString();
+                break;
+            case R.id.button3:
+                sMsg=b.getText().toString();
+                break;
+            case R.id.button4:
+                sMsg=b.getText().toString();
+                break;
+            case R.id.button5:
+                sMsg=b.getText().toString();
+                break;
+        }
+        Toast.makeText(this,String.format("Opening %s",sMsg),Toast.LENGTH_LONG).show();
+
+    }
+}
